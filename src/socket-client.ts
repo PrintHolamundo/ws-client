6b9cbb95b609de763e3daf944a7e563a350c4dee
PrintHolamundo <jdohca@gmail.com>
2023-02-05 20:06:38 -0600

    Emitir Cliente - Escuchar Servidor

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -11,6 +11,10 @@ export const connectToserver = () => {
 const addListeners = (socket: Socket) => {
     const serverStatusLabel = document.querySelector('#server-status')!;
     const clientsUl = document.querySelector('#clients-ul')!;
+
+    const messageForm = document.querySelector<HTMLFormElement>('#message-form')!;
+    const messageInput = document.querySelector<HTMLInputElement>('#message-input')!;
+
     socket.on('connect', () => {
         serverStatusLabel.innerHTML = 'connected';
     })
@@ -23,5 +27,15 @@ const addListeners = (socket: Socket) => {
             clientsHtml += `<li>${clientId}</li>`
         });
         clientsUl.innerHTML = clientsHtml
+    })
+    messageForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        if (messageInput.value.trim().length <= 0) return;
+        socket.emit('message-from-client', {
+            id: 'YO!!',
+            message: messageInput.value
+        })
+        messageInput.value = '';
+        console.log({ id: 'YO!!', message: messageInput.value })
     })
 }
